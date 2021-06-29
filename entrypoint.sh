3bfcccbccad99121c60b813f55da56b706c59d0c
Nathan <kwnath@gmail.com>
2021-06-29 23:00:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 output=$(theme $1 $2)
 
+echo "branch test: $branch"
 branch=$($3 | sed 's/\//-/g')
 echo "branch: $branch"
 themeId=$($output | grep "\[$branch\]" | xargs | grep -Eo "\[(\d+?)]" | cut -d "[" -f 2 | cut -d "]" -f -1)
