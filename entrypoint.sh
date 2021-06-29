fe9aae7233a1541298adc12ae242f7b1531ed6bc
Nathan <kwnath@gmail.com>
2021-06-29 22:48:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ output=$(theme $1 $2)
 
 if [ $3 ] && [ $1 == "get" ]; then
     branch=$($3 | sed 's/\//-/g')
-    themeId=$(output | grep "\[$branch\]" | xargs | grep -Eo "\[(\d+?)]" | cut -d "[" -f 2 | cut -d "]" -f -1)
+    themeId=$($output | grep "\[$branch\]" | xargs | grep -Eo "\[(\d+?)]" | cut -d "[" -f 2 | cut -d "]" -f -1)
 fi
 
 echo "::set-output name=result::$output"
