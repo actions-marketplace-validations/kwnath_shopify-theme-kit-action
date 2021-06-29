1da77a043d99b46f114af887336468148ab0e707
Nathan <kwnath@gmail.com>
2021-06-29 23:08:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,16 @@
 
 output=$(theme $1 $2)
 
-echo "branch test: $3"
-branch=$($3 | sed 's/\//-/g')
-echo "branch: $branch"
-themeId=$($output | grep "\[$branch\]" | xargs | grep -Eo "\[(\d+?)]" | cut -d "[" -f 2 | cut -d "]" -f -1)
-echo "branch: $branch"
-echo "themeId: $themeId"
+if [$3] && [$1 == "get"]; then
+  echo "branch test: $3"
+  branch=$($3 | sed 's/\//-/g')
+  echo "branch: $branch"
+  themeId=$($output | grep "\[$branch\]" | xargs | grep -Eo "\[(\d+?)]" | cut -d "[" -f 2 | cut -d "]" -f -1)
+  echo "branch: $branch"
+  echo "themeId: $themeId"
+  echo "::set-output name=themeId::$themeId"
+fi
 
-echo "::set-output name=themeId::$themeId"
 
 
 echo "::set-output name=result::$output"
