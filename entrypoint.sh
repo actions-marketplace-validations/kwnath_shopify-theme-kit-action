8033bf13a1ec1e205df392ddf048a5cb172baead
Nathan <kwnath@gmail.com>
2021-06-29 22:27:10 +0800

    Update for a specific usecase

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,13 @@
 
 output=$(theme $1 $2)
 
+
+getThemeId() {
+    themeId="$(theme get --list | grep "\[$1\]" | xargs | grep -Eo "\[(\d+?)]" | cut -d "[" -f 2 | cut -d "]" -f -1)"
+    echo $themeId
+}
+branch=$($3 | sed 's/\//-/g')
+themeId=$(getThemeId $branch)
+
 echo "::set-output name=result::$output"
+echo "::set-output name=themeId::$themeId"
